1472cd5db118f680be27cf7843af11fd057a0e5c
Alice Huang <alicehccn@gmail.com>
2022-05-14 13:16:02 -0700

    import lib module

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-	"fmt"
-	"bytes"
+	"kitchen/lib"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,25 +10,7 @@ func main() {
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"192.168.1.2"})
 	router.GET("/code", func(c *gin.Context) {
-		RunCode("node", "hello.js")
+		lib.RunCode("node", "hello.js")
 	})
 	router.Run("localhost:8080")
 }
-
-func RunCode(cmd string, input string) {
-	process := exec.Command(cmd, input)
-	stdin, err := process.StdinPipe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer stdin.Close()
-	buf := new(bytes.Buffer)
-	process.Stdout = buf
-	process.Stderr = os.Stderr
-
-	if err = process.Start(); err != nil {
-		fmt.Println(err)
-	}
-	process.Wait()
-	fmt.Println(buf)
-} 
